eba36dd6b6fc3f8b321176b58032b9ebed343862
Luis Cabrera Sauco <icorrecam@gmail.com>
2013-05-24 00:06:31 +0100

    Vista simple. Hará las funciones de index.html

--- cafeteria/views.py
+++ cafeteria/views.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
-def index(request):
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+
+def index_view(request):
     '''Ejemplo de vista básica.'''
 
-    return HttpResponse("Hello, world. You’re at the poll index.")
+    # return HttpResponse("Hello, world. You’re at the poll index.")
+
+    # html = "<html><head><title></title></head><body><p>Ejemplo de plantilla integrada.</p></body></html>"
+    # return HttpResponse(html)
+
+    # return render_to_response('index.html', {’latest_poll_list’: latest_poll_list})
+
+    return render_to_response('base.html',
+                              context_instance=RequestContext(request))
+
 
