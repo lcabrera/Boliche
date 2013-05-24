7a8e97f44e9188c7b563040794c3052a01a0ccbb
Luis Cabrera Sauco <icorrecam@gmail.com>
2013-05-24 14:17:22 +0100

    Añadido South en la carga de aplicaciones.

--- cafeteria/settings.py
+++ cafeteria/settings.py
@@ -134,7 +134,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
-    'South',
+    'south',
 )
 
 # A sample logging configuration. The only tangible logging
