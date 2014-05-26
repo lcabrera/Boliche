1dac48af7a59c986a42b5c3c4556af08fe7fd8d8
Luis Cabrera Sauco <icorrecam@gmail.com>
2014-05-26 15:07:48 +0100

    Desactivada la barra de depuración. Añadida librería para soporte de Google Calendar.

--- cafeteria/settings.d/debug_toolbar.py
+++ cafeteria/settings.d/debug_toolbar.py
@@ -4,12 +4,12 @@
 
 '''
 
-if DEBUG and not PRODUCCION:
-    MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware', )
-    INSTALLED_APPS += ('debug_toolbar', )
-    DEBUG_TOOLBAR_CONFIG = {
-        'INTERCEPT_REDIRECTS': False,
-    }
-    INTERNAL_IPS = ('127.0.0.1', )
-
+#if DEBUG and not PRODUCCION:
+#    MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware', )
+#    INSTALLED_APPS += ('debug_toolbar', )
+#    DEBUG_TOOLBAR_CONFIG = {
+#        'INTERCEPT_REDIRECTS': False,
+#    }
+#    INTERNAL_IPS = ('127.0.0.1', )
+#
 
