8d1d9f53007b6d94b67a06de59367b6125a05609
Luis Cabrera Sauco <icorrecam@gmail.com>
2014-06-20 11:35:44 +0100

    Eliminados retornos de carro.

--- manage.py
+++ manage.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import os
 import sys
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cafeteria.settings")
+if __name__ == '__main__':
+
+    os.environ.setdefault(
+        'DJANGO_SETTINGS_MODULE',
+        'cafeteria.settings'
+    )
 
     from django.core.management import execute_from_command_line
 
