14c3282e7d954566b09e278162f6e5e51ce5cff2
Luis Cabrera Sauco <icorrecam@gmail.com>
2014-06-22 17:18:50 +0100

    Subimos el valor de depuración.

---
+++ cafeteria/settings.d/DEFAULT_MESSAGE_LEVELS.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+DEFAULT_MESSAGE_LEVELS = DEBUG
+
+#'DEFAULT_MESSAGE_LEVELS': {
+#    'DEBUG': 10,
+#    'ERROR': 40,
+#    'INFO': 20,
+#    'SUCCESS': 25,
+#    'WARNING': 30},
+
+
